756e27a4bb7f4e0b38c5d78f7c855195587a5446
Vitor Rubim <vitorrubim1@hotmail.com>
2020-02-25 13:33:54 -0300

    Fazendo com que quando o audio finalize, inicie o próximo da lista

--- base/index.js
+++ base/index.js
@@ -1,3 +1,8 @@
+//RETORNAR O FILES COM OS ARQUIVOS DO (data.js)
+const path = function(file){
+  return `files/${file}`
+}
+
 //AQUI ESPERO O PLAYER CARREGAR POR INTEIRO PARA INICIAR
 window.addEventListener("load", player.start());
 
--- base/player.js
+++ base/player.js
@@ -5,22 +5,41 @@ window.player = {
     artist: document.querySelector(".artist"),
     audio: document.querySelector("audio"),
 
-    //ALIMENTANDO COM INFORMAÇÕES
-    data: {
-        title:
-        "Como começei a programar / Por que criamos a Rocketseat / Nossa Stack",
-        artist: "Diego Fernandes",
-        cover: "files/como-comecei.jpg",
-        file: "files/como-comecei.mp3"
+    //PEGANDO INFORMAÇÕES DO OBJETO DE DENTRO DO(data.js)
+    audioData: audios,
+
+    currentAudio: {},
+    //PARA INFORMAR QUE O AUDIO INICIAL É O PRIMEIRO DO ARRAY
+    currentPlaying: 0,
+
+    start(){
+        //SETANDO INFORMAÇÕES
+        this.update();
+
+        //QUANDO O AUDIO FINALIZAR, IR PARA O PRÓXIMO DA LISTA
+        this.audio.onended = () => this.next();
     },
-    
-    start: function(){
+    //FUNCÃO DE AVANÇAR ÁUDIO
+    next(){
+        //ADICIONANDO MAIS UM, PARA AVANÇAR O ÁUDIO
+        this.currentPlaying++;
+        this.update();
+        this.audio.play();
+    },
+
+    //FUNÇÃO DE ATUALIZAR DADOS DO PLAYER, IMAGEM, ARTISTA ETC..
+    update(){
+        //ALIMENTANDO COM INFORMAÇÕES
+        this.currentAudio = this.audioData[this.currentPlaying];
+
         //MANIPULANDO O STYLE
-        this.cover.style.background = `url('${this.data.cover}') no-repeat center center / cover`;
+        this.cover.style.background = `url('${path(
+            this.currentAudio.cover)}') no-repeat center center / cover`;
         //INSERINDO TEXTO E AÚDIO
-        this.title.innerText = this.data.title;
-        this.artist.innerText = this.data.artist;
-        this.audio.src = this.data.file
+        this.title.innerText = this.currentAudio.title;
+        this.artist.innerText = this.currentAudio.artist;
+        this.audio.src = path(this.currentAudio.file);
     }
+
 };
     
