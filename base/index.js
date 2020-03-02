88a513c1bb8d3a4b65d754405f52c7c21ed0de7b
Vitor Rubim <vitorrubim1@hotmail.com>
2020-03-01 21:58:35 -0300

    Importando arquivos e exportando com ES6

--- base/index.js
+++ base/index.js
@@ -1,7 +1,4 @@
-//RETORNAR O FILES COM OS ARQUIVOS DO (data.js)
-const path = function(file){
-  return `files/${file}`
-}
+import player from './player.js';
 
 //AQUI ESPERO O PLAYER CARREGAR POR INTEIRO PARA INICIAR
 window.addEventListener("load", player.start());
---
+++ base/utils.js
@@ -0,0 +1,8 @@
+//RETORNAR O FILES COM OS ARQUIVOS DO (data.js)
+const path = function(file){
+    return `files/${file}`
+}
+
+export {
+    path
+};
